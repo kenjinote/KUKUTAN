1ad2ba611737ce5263ab6fffdac8b3013de370bf
kenjinote <kenji256@gmail.com>
2020-05-12 06:22:52 +0900

    段を設定するようにした

--- TopPage.xaml.cs
+++ TopPage.xaml.cs
@@ -30,12 +30,6 @@ namespace KUKUTAN
             mainWindow.Close();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            // 出題設定に遷移
-            NavigationService.Navigate(new syutsudaisettei());
-        }
-
         private void anaumebutton_Click(object sender, RoutedEventArgs e)
         {
             // 段設定に遷移
@@ -56,6 +50,87 @@ namespace KUKUTAN
             {
                 NavigationService.Navigate(new settei());                
             }
+        }
+
+        // 1の段 ボタンが押されたとき
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Module1.dan = 1; // 1の段
+
+            // 出題設定画面に遷移
+            NavigationService.Navigate(new syutsudaisettei());
+        }
+
+        // 2の段 ボタンが押されたとき
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Module1.dan = 2; // 2の段
+
+            // 出題設定画面に遷移
+            NavigationService.Navigate(new syutsudaisettei());
+        }
+
+        // 3の段 ボタンが押されたとき
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            Module1.dan = 3; // 3の段
+
+            // 出題設定画面に遷移
+            NavigationService.Navigate(new syutsudaisettei());
+        }
+
+        // 4の段 ボタンが押されたとき
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            Module1.dan = 4; // 4の段
+
+            // 出題設定画面に遷移
+            NavigationService.Navigate(new syutsudaisettei());
+        }
+
+        // 5の段 ボタンが押されたとき
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            Module1.dan = 5; // 5の段
+
+            // 出題設定画面に遷移
+            NavigationService.Navigate(new syutsudaisettei());
+        }
+
+        // 6の段 ボタンが押されたとき
+        private void button6_Click(object sender, RoutedEventArgs e)
+        {
+            Module1.dan = 6; // 6の段
+
+            // 出題設定画面に遷移
+            NavigationService.Navigate(new syutsudaisettei());
+        }
+
+        // 7の段 ボタンが押されたとき
+        private void button7_Click(object sender, RoutedEventArgs e)
+        {
+            Module1.dan = 7; // 7の段
+
+            // 出題設定画面に遷移
+            NavigationService.Navigate(new syutsudaisettei());
+        }
+
+        // 8の段 ボタンが押されたとき
+        private void button8_Click(object sender, RoutedEventArgs e)
+        {
+            Module1.dan = 8; // 8の段
+
+            // 出題設定画面に遷移
+            NavigationService.Navigate(new syutsudaisettei());
+        }
+
+        // 9の段 ボタンが押されたとき
+        private void button9_Click(object sender, RoutedEventArgs e)
+        {
+            Module1.dan = 9; // 9の段
+
+            // 出題設定画面に遷移
+            NavigationService.Navigate(new syutsudaisettei());
         }
     }
 }
